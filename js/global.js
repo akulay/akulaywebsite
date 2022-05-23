4cc8cf539dfbda2287739867a85258ee6776a7ad
Akulay <takimakulay@gmail.com>
2022-05-23 22:28:05 +0300

    1

---
+++ js/global.js
@@ -0,0 +1,25 @@
+/*var ax = 15;
+var ay = 15;
+var cnvs = document.getElementById("head-cnvs");
+var ctx = cnvs.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = "white";
+ctx.arc(ax, ay, 5, 0, 2 * Math.PI);
+ctx.fillStyle = "white";
+ctx.fill();
+ctx.stroke();
+
+function a(){
+    ctx.clearRect(0, 0, cnvs.width, cnvs.height);
+    ctx.arc(ax, ay, 5, 0, 2 * Math.PI);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.stroke();
+}
+
+for (i = 0; i<30; i++){
+    ax += 1;
+    ay += 0.5;
+    a();
+}
+*/
